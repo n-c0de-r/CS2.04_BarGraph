28cb51943f9010e1e8d88e48b6d1d4b14c5ea9b4
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-05-08 22:55:33 +0200

    Added JDoc comments, relative scale
    Added a helper method, that finds the highest value in the array and uses this as the maximum scale, making the histogram always acurate for any number of characters in total. avoiding hardcoded limits!

--- CountChars.java
+++ CountChars.java
@@ -1,4 +1,12 @@
 import java.io.*;
+/**
+ * This class takes an input text file, iterates over every single character
+ * and counts their occurrence. Only ASCII characters from A-Z are considered.
+ * all others are omitted.
+ * @author Sönke
+ * @author Nermin
+ * @version 08.05.2021
+ */
 
 public class CountChars {
 
@@ -8,25 +16,30 @@ public class CountChars {
 	private File original;
 	private File save;
 	private int[] letters;
-
+	
 	public static void main(String[] args) {
+		//Create an instance of the class for OOP approach
 		CountChars counter = new CountChars();
-
+		
 		counter.setup();
 		counter.readChars(counter.text);
 		counter.printHistogram();
 	}
-
+	
+	/**
+	 * Creating all the relevant objects for the class
+	 */
 	private void setup() {
 		original = new File("./original.txt");
-		save = new File("./output.txt");
+		save = new File("./frequency.txt");
 		letters = new int[27];
-
+		
 		try {
 			input = new FileReader(original);
 			output = new FileWriter(save);
 			text = new BufferedReader(input);
 			save.createNewFile();
+		
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -35,7 +48,11 @@ public class CountChars {
 			e.printStackTrace();
 		}
 	}
-
+	
+	/**
+	 * Reads each character of a given text file
+	 * @param text		Text from a text file in a buffered reader
+	 */
 	private void readChars(BufferedReader text) {
 		try {
 			int c = 0;
@@ -63,33 +80,52 @@ public class CountChars {
 					letters[0] = letters[0] + 1;
 				}
 			}
-
+		
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
+	
+	/**
+	 * prints out a histogram where stars represent a
+	 * certain amount of characters relative to the total.
+	 */
 	private void printHistogram() {
 		String star = "*";
 		//Start with a text stating the total number, not vital but fun
 		String str = "Total letters: " + letters[0] + "\r\n\r\n";
+		int percent = (findMax(letters, 1, letters.length) /100);
 		for (int x = 1; x<letters.length; x++) {
 			//Check each value in the array and set a star
 			//for each 10 characters.
 			char c = (char) (x+64);
-			str += c + ": " + star.repeat(letters[x] / 10) + "\r\n";
+			str += c + ": " + star.repeat(letters[x] / percent) + "\r\n";
 		}
 		//Append a legend/description at the end
-		str += "\r\n'*' = represents 10 characters of each letter";
+		str += "\r\n'*' = represents " + percent + " characters of each letter";
 		try {
 			output.write(str);
 			output.flush();
 			output.close();
-
+		
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Finds the highest number of a letter, 
+	 * using this as the scale maximum. Recursive function.
+	 * @param values	array to check
+	 * @param start		where to begin checking
+	 * @param value		where to finish
+	 * @return		the highest number found
+	 */
+	public static int findMax(int[] values, int start, int value)
+	{
+	   if (start == values.length) return value; 
+	   else return findMax(values, start + 1, Math.max(value, values[start]));
 	}
 }
