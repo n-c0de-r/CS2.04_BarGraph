bed0fcc089b7b8a168414ef1b956f349a23d20c5
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-05-08 21:05:47 +0200

    Added character counter into an array and a write method
    checks each character's ASCII value and increments the value at this position.
    Also added a simple write method to make stars.

--- CountChars.java
+++ CountChars.java
@@ -7,18 +7,21 @@ public class CountChars {
 	private BufferedReader text;
 	private File original;
 	private File save;
+	private int[] letters;
 
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		CountChars counter = new CountChars();
-		
+
 		counter.setup();
 		counter.readChars(counter.text);
+		counter.printHistogram();
 	}
-	
+
 	private void setup() {
 		original = new File("./original.txt");
 		save = new File("./output.txt");
-		
+		letters = new int[27];
+
 		try {
 			input = new FileReader(original);
 			output = new FileWriter(save);
@@ -32,27 +35,57 @@ public class CountChars {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void readChars(BufferedReader text) {
 		try {
 			int c = 0;
 			while ((c = text.read()) != -1) {
+				// Check the ASCII code of letters
+				if (c > 96 && c < 123) {
+					//convert to upper case
+					c -= 32;
+				}
+				//Skip all line feeds & carriage returns
 				if (c == 10 || c == 13) {
 					text.skip(1);
 				} else {
-					char character = (char) c; // converting integer to char
-					System.out.println(character);
-					writeChars(character);
+					//only count upper case letters
+					if (c > 64 && c < 90) {
+						//increment the counted letter in it's respective
+						//position in the array of letters.
+						letters[c - 64] = letters[c - 64] + 1;
+					} else {
+						//skip everything else
+						text.skip(1);
+					}
+					//for every counted letter increment the number
+					//of all letters found in a file
+					letters[0] = letters[0] + 1;
 				}
 			}
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	private void writeChars(char c) throws IOException {
-		output.write(c);
+
+	private void printHistogram() {
+		String star = "*";
+		String str = "";
+		for (int x : letters) {
+			//Check each value in the array and set a star
+			//for each 10 characters.
+			str += star.repeat(x / 10) + "\r\n";
+		}
+		try {
+			output.write(str);
+			output.flush();
+			output.close();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
 }
